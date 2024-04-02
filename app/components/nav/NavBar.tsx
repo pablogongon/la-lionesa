940cac909639da7fc5277122d379d69c6ce4628f
pablogongon <165203139+pablogongon@users.noreply.github.com>
2024-04-02 23:52:24 +0200

    Update NavBar.tsx

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -35,12 +35,13 @@ const NavBar = async () => {
           md:gap-0
           "
           >
-            <Link
-              href="/"
-              className={`${redressed.className} font-bold text-2xl text-[#B36F09]`}
-            >
-              La Lionesa
-            </Link>
+            <a href="/">
+              <img
+                src="/logo.png"
+                alt="Descripción de la imagen"
+                className={`${redressed.className} font-bold text-2xl text-[#B36F09] max-w-[120px] max-h-[120px]`}
+              />
+            </a>
             <div className="hidden md:block"><SearchBar/></div>
             <div className="flex items-center gap-8 md:gap-12">
               <CartCount />
