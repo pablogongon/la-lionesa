9c92c66082b4bdeffe81f878428df806918fa110
pablogongon <165203139+pablogongon@users.noreply.github.com>
2024-04-03 15:58:34 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
@@ -12,13 +11,12 @@ const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 export const metadata: Metadata = {
   title: "La Lionesa",
   description: "Pagina web Oficial La Lionesa profesional components",
+  icons:{
+    icon:["favicon.ico?v=4"],
+    apple:["/apple-touch-icon.png"],
+    shortcut:["/apple-touch-icon.png"]
+  }
 };
-
-
-interface ChatbotScriptProps extends React.ScriptHTMLAttributes<HTMLScriptElement> {
-  chatbotId: string;
-  domain: string;
-}
 
 export default async function RootLayout({
   children,
@@ -28,27 +26,6 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={`${poppins.className} text-white`}>
-        
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.embeddedChatbotConfig = {
-                chatbotId: "iI0WBS8d-86ubE3k6pOK4",
-                domain: "www.chatbase.co"
-              };
-            `,
-          }}
-        />
-        <script
-          src="https://www.chatbase.co/embed.min.js"
-          {...{
-            chatbotId: "iI0WBS8d-86ubE3k6pOK4",
-            domain: "www.chatbase.co",
-            defer: true,
-          } as ChatbotScriptProps} 
-        ></script>
-        
-
         <Toaster
           toastOptions={{
             style: {
@@ -61,6 +38,7 @@ export default async function RootLayout({
           <div className="flex flex-col min-h-screen bg-[#212121] ">
             <NavBar />
             <main className="flex-grow">{children}</main>
+            
             <Footer />
           </div>
         </CartProvider>
